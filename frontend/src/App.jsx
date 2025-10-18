b75d54172ed1996ec9192821de8831478ec5b70e
zohaibfast99 <zohaibrashid999@gmail.com>
2025-10-18 23:42:06 +0500

    Created main routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import SettingsPage from "./pages/SettingsPage"
+import LoginPage from "./pages/LoginPage"
+import ProfilePage from "./pages/ProfilePage"
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      Hello
+    <div>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} /> 
+        <Route path="/signup" element={<SignUpPage />} /> 
+        <Route path="/login" element={<LoginPage />} /> 
+        <Route path="/settings" element={<SettingsPage />} /> 
+        <Route path="/profile" element={<ProfilePage />} /> 
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUpPage = () => {
+  return (
+    <div>
+      Signup page
+    </div>
+  )
+}
+
+export default SignUpPage
